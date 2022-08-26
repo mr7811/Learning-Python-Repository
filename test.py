419b8e475702805337b4a161ce16657a1e7fff6f
Timothy Tang <timothytang@Timothys-MacBook-Pro.local>
2022-08-26 14:05:12 -0400

    palindrome problem

---
+++ test.py
@@ -0,0 +1,15 @@
+#Use a program to test if string is a palindrome.
+#status: completed
+
+# pal = input("write a word")
+# y =[]
+
+# for c in pal:
+#     y.append(c)
+
+# z= y[::-1]
+# h= y[:]
+# if h == z:
+#     print("yes")
+# else:
+#     print("no not a palindrome")
