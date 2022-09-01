e5c3729ab0d4480fa981530311490f648ce8ec41
Timothy Tang <timothytang@Timothys-MacBook-Pro.local>
2022-09-01 12:43:21 -0400

    renamed sections based off questions

---
+++ 11to20.py
@@ -0,0 +1,22 @@
+# num = int(input("type a number to find if it is prime"))
+
+# listRange = list(range(1,num+1))
+
+# divisorList=[]
+
+# for number in listRange:
+#     if num % number == 0:
+#         divisorList.append(number)
+
+# if len(divisorList) > 2:print("it is not prime")
+# else: print("it is prime")
+
+#type  a string and get it backwords problem
+
+# ask_string = input("Type in a string and get it backwards")
+
+# y = ask_string.split(" ")
+    
+# z= y[::-1]
+
+# print(z)
